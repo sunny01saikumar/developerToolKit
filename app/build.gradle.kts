dacd8eca618e830bbc45d2829f437485d1186ce5
Sai Kumar <saikumar13637@gmail.com>
2026-08-02 05:28:45 +0530

    chore: upgrade target SDK to 36 and increment version to 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,14 +8,14 @@ plugins {
 
 android {
     namespace = "com.devtoolkit.pro"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.devtoolkit.pro"
         minSdk = 26
-        targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        targetSdk = 36
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
