176364b885402425fc74548027796266b52f14ac
Sai Kumar <saikumar13637@gmail.com>
2026-08-02 05:35:36 +0530

    chore: untrack build cache and compiled binaries

--- app/build/generated/hilt/component_trees/release/dagger/hilt/internal/processedrootsentinel/codegen/_com_devtoolkit_pro_DevToolkitApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.devtoolkit.pro.DevToolkitApp"
-)
-public final class _com_devtoolkit_pro_DevToolkitApp {
-}
